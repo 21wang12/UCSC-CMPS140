6c5c1b4970e4ac58d6257c68130f2fd7c33bb229
Wang Shuo <wangshuo@Wangs-MacBook-Pro.local>
2023-01-29 01:29:32 +0800

    solve dfs and bfs

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -6,6 +6,10 @@ the agent from the start to the goal. These actions all have to be legal moves (
 no moving through walls).
 """
 
+from pacai.util.stack import Stack
+from pacai.util.priorityQueue import PriorityQueue
+from pacai.util.queue import Queue
+
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first [p 85].
@@ -21,17 +25,57 @@ def depthFirstSearch(problem):
     print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
     ```
     """
-
-    # *** Your Code Here ***
-    raise NotImplementedError()
+    start = (problem.startingState(), None, 0)
+    if problem.isGoal(start[0]):
+        return []
+    frontier = Stack()
+    frontier.push(start)
+    visited = set()
+    memory = dict()
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        visited.add(node[0])
+        for neighbor in problem.successorStates(node[0]):
+            if neighbor[0] not in visited:
+                memory[neighbor] = node
+                if problem.isGoal(neighbor[0]):
+                    path = []
+                    # constrcut the path
+                    parent = neighbor
+                    while parent[0] != start[0]:
+                        path.append(parent[1])
+                        parent = memory[parent]
+                    return list(reversed(path))
+                frontier.push(neighbor)
+    return []
 
 def breadthFirstSearch(problem):
     """
     Search the shallowest nodes in the search tree first. [p 81]
     """
-
-    # *** Your Code Here ***
-    raise NotImplementedError()
+    start = (problem.startingState(), None, 0)
+    if problem.isGoal(start[0]):
+        return []
+    frontier = Queue()
+    frontier.push(start)
+    visited = set()
+    memory = dict()
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        visited.add(node[0])
+        for neighbor in problem.successorStates(node[0]):
+            if neighbor[0] not in visited:
+                memory[neighbor] = node
+                if problem.isGoal(neighbor[0]):
+                    path = []
+                    # constrcut the path
+                    parent = neighbor
+                    while parent[0] != start[0]:
+                        path.append(parent[1])
+                        parent = memory[parent]
+                    return list(reversed(path))
+                frontier.push(neighbor)
+    return []
 
 def uniformCostSearch(problem):
     """
